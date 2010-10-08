719e200e3949d46c5eba1da5c8cf038abaa17824
pex <rernst@farbenmeer.net>
2010-10-08 15:45:34 +0200

    Rails3 Fix for acts_as_mappapble -> added merge_conditions

--- lib/geokit-rails/acts_as_mappable.rb
+++ lib/geokit-rails/acts_as_mappable.rb
@@ -71,6 +71,19 @@ module Geokit
             end
           end
         end
+        
+        def merge_conditions(*conditions)
+          segments = []
+
+          conditions.each do |condition|
+            unless condition.blank?
+              sql = sanitize_sql(condition)
+              segments << sql unless sql.blank?
+            end
+          end
+
+          "(#{segments.join(') AND (')})" unless segments.empty?
+        end
       end
     end # Glue
     
